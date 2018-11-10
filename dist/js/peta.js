af4e183996b84d64e34ec4ba2583234818cf4c69
rizanw <rzkandre@gmail.com>
2018-11-11 04:35:29 +0700

    update dengan json

--- dist/js/peta.js
+++ dist/js/peta.js
@@ -33,62 +33,21 @@ let par = document.createElement("p");
 gmb.appendChild(img);
 rev.appendChild(par);
 
-let r0 = "restoran spanyol di jakarta yang dekat dengan kantor saya";
-let r1 = "warung kopi cita rasa yang sangat tinggi dengan harga yang murah";
-let r2 = "Ikan bakar kualitas tinggi, hampir gosong tapi belum";
-let r3 = "Steak lokal harga impor, 200gr dan 300gr mentah ";
-let r4 = "seafood international lobster, king crabs, cumi, kerang, semua ada";
+const URL = "asset/peta.json";
 
-let places = [
-    {
-        "lokasi": [-7.266712, 112.796374],
-        "restoran": "McDonalds Mulyosari",
-        "gambar": "asset/pict/mcdmulyos.jpg",
-        "review": "restoran spanyol di jakarta yang dekat dengan kantor saya"
-    },
-    {
-        "lokasi": [-7.266446, 112.796334],
-        "restoran": "KFC Mulyosari",
-        "gambar": "asset/pict/mcdmulyos.jpg",
-        "review": r0
-    },
-    {
-        "lokasi": [-7.272629, 112.797207],
-        "restoran": "PHD Mulyosari",
-        "gambar": "asset/pict/mcdmulyos.jpg",
-        "review": r0
-    },
-    {
-        "lokasi": [-7.266039, 112.796194],
-        "restoran": "PizzaHut Mulyosari",
-        "gambar": "asset/pict/mcdmulyos.jpg",
-        "review": r0
-    },
-    {
-        "lokasi": [-7.280249, 112.774577],
-        "restoran": "Carl's Jr Kertajaya",
-        "gambar": "asset/pict/mcdmulyos.jpg",
-        "review": r0
-    },
-    {
-        "lokasi": [-7.279949, 112.769219],
-        "restoran": "Richeese Factory Manyar",
-        "gambar": "asset/pict/mcdmulyos.jpg",
-        "review": r0
-    },
-    {
-        "lokasi": [-7.279622, 112.766367],
-        "restoran": "PizzaHut Manyar",
-        "gambar": "asset/pict/mcdmulyos.jpg",
-        "review": r0
-    },
-    {
-        "lokasi": [-7.279470, 112.765328],
-        "restoran": "McDonalds Manyar",
-        "gambar": "asset/pict/mcdmulyos.jpg",
-        "review": r0
-    },
-];
+async function f(url) {
+    try {
+        let response = await (fetch(url));
+        let data = await response.json();
+        localStorage.setItem("places", data.places);
+    }catch (e) {
+        console.log(e);
+    }
+}
+
+f(URL);
+
+let places = JSON.parse(localStorage.getItem("places"));
 
 for (var p of places) {
     var marker = L.marker(p.lokasi).addTo(mymap).bindPopup(p.restoran);
